1dfacd2a4327dace080d67a2d39b799ed22a1f7d
Rasheed Ayoade <rashotech@gmail.com>
2022-05-31 16:25:28 +0100

    Typescript types

---
+++ src/interfaces/AccountInterface.ts
@@ -0,0 +1,8 @@
+export interface AccountInterface {
+    id: number
+    account_number: string;
+    user_id: number;
+    account_balance: number;
+    account_type: string;
+    account_status: string;
+}
